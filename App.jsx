9d78c14b6af301966968c4656a713d451659a72f
arctic-shark <fahimshareear@gmail.com>
2025-12-04 15:15:22 +0600

    Adding toggling effect in the main app componant for the Available players and Selected players

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import './App.css'
 import AvailablePlayer from './componants/Availableplayers/AvailablePlayer';
 import Navbar from './componants/Navbar/Navbar';
@@ -12,14 +12,24 @@ const fetchPlayers = async () =>{
 
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   const playerPromise = fetchPlayers();
   return (
     <>
     <Navbar></Navbar>
-    <Suspense fallback={<span className="loading loading-infinity loading-xl flex items-center mx-auto"></span>}>
-      <AvailablePlayer playerPromise={playerPromise}></AvailablePlayer>
-    </Suspense>
-    <SelectedPlayers></SelectedPlayers>
+    <div className='max-w-[1200px] mx-auto flex justify-between items-center'>
+      <h1 className='font-bold'>Available Players</h1>
+      <div className='flex items-center gap-0'>
+        <button className={`p-2 text-[15px] font-medium border border-gray-400 rounded-l-xl border-r-0 ${toggle === true? "bg-[#E7FE29]" : ""}`} onClick={() => setToggle(true)}>Available</button>
+        <button className={`p-2 text-[15px] font-medium border border-gray-400 rounded-r-xl border-l-0 ${toggle === false? "bg-[#E7FE29]": ""}`} onClick={() => setToggle(false)}>Selected <span>(0)</span></button>
+      </div>
+    </div>
+
+    {
+      toggle === true ? <Suspense fallback={<span className="loading loading-infinity loading-xl flex items-center mx-auto"></span>}>
+                        <AvailablePlayer playerPromise={playerPromise}></AvailablePlayer>
+                      </Suspense> : <SelectedPlayers></SelectedPlayers>
+    }
     </>
   )
 }
