33f11b67b345540821afe12d6c36df26c6ecfe4b
arctic-shark <fahimshareear@gmail.com>
2025-12-05 00:44:42 +0600

    Adding banner button style

--- componants/Navbar/Navbar.jsx
+++ componants/Navbar/Navbar.jsx
@@ -19,14 +19,14 @@ const Navbar = ({availBalance}) => {
                     <img src={dollarImg} alt="" />
                 </div>
             </div>
-            <div className='banner max-w-[1200px] mx-auto border-2 border-red-500 bg-black rounded-2xl mt-10'>
+            <div className='banner max-w-[1200px] mx-auto  bg-black rounded-2xl mt-10'>
                 <img src={bannerBg} alt="" className='rounded-2xl'/>
                 <div className="banner_content">
                     <img src={bannerMain} alt="" />
                     <h1>Assemble Your Ultimate Dream 11 Cricket Team</h1>
                     <p>Beyond Boundaries Beyond Limits</p>
-                    <div className='w-[240px] flex items-center justify-center h-[70px] border-2 border-yellow-500 mt-8 rounded-3xl'>
-                        <button className='text-2xl text-black font-bold p-2 bg-yellow-500 rounded-2xl'>Claim Free Credit</button>
+                    <div className='w-[240px] flex items-center justify-center h-[70px] border-2 border-yellow-500 mt-8 rounded-2xl'>
+                        <button className='btn text-2xl text-black font-bold p-2 cursor-pointer bg-yellow-500 rounded-1xl'>Claim Free Credit</button>
                     </div>
                 </div>
             </div>
